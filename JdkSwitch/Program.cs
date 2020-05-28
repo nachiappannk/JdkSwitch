2794385d8d2029a404de79db2d82c5dec828c497
Nachiappan NK <nachiappan.nk@mdoc.one>
2020-05-29 01:45:27 +0530

    Completed Jdk Switch

--- JdkSwitch/Program.cs
+++ JdkSwitch/Program.cs
@@ -1,57 +1,48 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace JdkSwitch
 {
     class Program
     {
+        const string ENVIRONMENT_VARIABLE_PREFIX = "JAVA_SDK_";
+        const string ENVIRONMENT_VARIABLE_TO_SET = "JAVA_HOME";
+
+        static IEnumerable<string> GetVariableNames()
+        {
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+            {
+                var key = de.Key.ToString();
+                if (!key.StartsWith(ENVIRONMENT_VARIABLE_PREFIX)) continue;
+                yield return de.Key.ToString().Replace(ENVIRONMENT_VARIABLE_PREFIX, "");
+            }
+        }
         static void Main(string[] args)
         {
-            string text = File.ReadAllText("config.json");
-            var dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(text);
-            var path = Environment.GetEnvironmentVariable("path", EnvironmentVariableTarget.Machine);
-
-            Console.WriteLine("The current path is " + path);
-
-            foreach (var kv in dictionary)
-            {
-                var item = kv.Value + "\\bin";
-                path = path.Replace(item + ";", "");
-                path = path.Replace(item, "");
-            }
-
+            var variables = GetVariableNames().ToList();
             Console.WriteLine("Please select the version");
             Console.WriteLine("0\t:EXIT");
-            var keys = dictionary.Keys.ToList();
-            for (int i = 0; i < keys.Count; i++)
+            for (int i = 0; i < variables.Count; i++)
             {
-                Console.WriteLine($"{i + 1}\t:{keys[i]}");
+                Console.WriteLine($"{i + 1}\t:{variables[i]}");
             }
 
             var input = Console.ReadLine();
             var intInput = int.Parse(input);
-
             if (intInput == 0)
             {
                 Console.WriteLine("Exiting the application due to user input");
                 Environment.Exit(0);
             }
 
-            var key = keys[intInput - 1];
-
-            var value = dictionary[key];
-            path = $"{value.TrimEnd('\\')}\\bin;{path}";
-            Console.WriteLine($"using {value}");
-
-            Environment.SetEnvironmentVariable("JAVA_HOME", value, EnvironmentVariableTarget.Machine);
-
-            Environment.SetEnvironmentVariable("PATH", path, EnvironmentVariableTarget.Machine);
-
+            var key = variables[intInput - 1];
+            Console.WriteLine($"using {key}");
+            var environmentVariableName = $"{ENVIRONMENT_VARIABLE_PREFIX}{key}";
+            var environmentVariableValue = Environment.GetEnvironmentVariable(environmentVariableName, EnvironmentVariableTarget.Machine);
+            Console.WriteLine($"Writing {environmentVariableValue}");
+            Environment.SetEnvironmentVariable(ENVIRONMENT_VARIABLE_TO_SET, environmentVariableValue, EnvironmentVariableTarget.Machine);
             Console.WriteLine("Bye... Press any key");
             Console.ReadKey();
 
